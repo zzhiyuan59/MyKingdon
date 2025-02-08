d16e7df733ce394b634235e2b5b341836c8ecffe
zzhiyuan <zzhiyuan@vt.edu>
2025-02-08 15:52:10 -0500

    finish v1

---
+++ Assets/UnitMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitMovement : MonoBehaviour
+{
+    Camera cam;
+    NavMeshAgent agent;
+
+    public LayerMask ground;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ground))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+
+}
