b5141b0b2e88f9b0bbb5699b2aae4a8f84a19fe9
zzhiyuan <zzhiyuan@vt.edu>
2025-02-08 16:58:14 -0500

    v2 partial selection workable

--- Assets/UnitSelectionManager.cs
+++ Assets/UnitSelectionManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
 
 public class UnitSelectionManager : MonoBehaviour
 {
@@ -9,6 +10,11 @@ public class UnitSelectionManager : MonoBehaviour
     public List<GameObject> allUnitsList = new List<GameObject>();
     public List<GameObject> unitsSelected = new List<GameObject>();
 
+    public LayerMask clickable;
+    public LayerMask ground;
+
+    public GameObject groundmarker;
+    private Camera cam;
 
     private void Awake()
     {
@@ -20,5 +26,47 @@ public class UnitSelectionManager : MonoBehaviour
         {
             Instance = this;
         }
+    }
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, clickable))
+            {
+                SelectByClicking(hit.collider.gameObject);
+            }
+            else
+            {
+                DeselectAll();
+            }
+        }
+    }
+
+    private void DeselectAll()
+    {
+        // throw new System.NotImplementedException();
+    }
+
+    private void SelectByClicking(GameObject unit)
+    {
+        DeselectAll();
+
+        unitsSelected.Add(unit);
+
+        EnableUnitMovement(unit, true);
+    }
+
+    private void EnableUnitMovement(GameObject unit, bool moveable)
+    {
+        unit.GetComponent<UnitMovement>().enabled = moveable;
     }
 }
